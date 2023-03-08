7dcf6cd1ab19fd387e8199db4d0bc9d16b7b47d6
Diego <diegolopezreduello@gmail.com>
2023-03-08 09:52:12 +0100

    Creado backend

---
+++ VinylShop/src/test/java/com/grupod/vinylshop/VinylShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupod.vinylshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinylShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
