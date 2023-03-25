e0574d90cd9e94916d2e5565d5c372f629c33bb3
Diego <diegolopezreduello@gmail.com>
2023-03-25 19:07:45 +0100

    Final primera iteracion

--- Skiffle/js/shop.js
+++ Skiffle/js/shop.js
@@ -1,32 +1,136 @@
 const vinyls = [
 
-    {
-        "id": 1,
-        "image": "https://i.discogs.com/N4Euq7BBUMVwWmlskftJ_vZtBXC71LVwwtbi0gvjoV4/rs:fit/g:sm/q:90/h:600/w:599/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTExODg4/MDEtMTQ3NTc3MDI1/MC0zNjkyLmpwZWc.jpeg",
-        "title": "Delta and a thousand Blades",
-        "price": "20$"
-    },
+  {
+    "id": 1,
+    "image": "https://i.discogs.com/N4Euq7BBUMVwWmlskftJ_vZtBXC71LVwwtbi0gvjoV4/rs:fit/g:sm/q:90/h:600/w:599/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTExODg4/MDEtMTQ3NTc3MDI1/MC0zNjkyLmpwZWc.jpeg",
+    "title": "Delta and a thousand Blades",
+    "price": "50$"
+  },
 
-    {
-        "id": 2,
-        "image": "https://i.discogs.com/BLb2qpCGiJ90OLh389AwftGVpJC3_S2UyzM-OJtnRKc/rs:fit/g:sm/q:90/h:598/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTEyODM2/NzI4LTE1ODc4MjQw/MDUtMzU5Mi5wbmc.jpeg",
-        "title": "Delta's fury",
-        "price": "20$"
-    },
+  {
+    "id": 5,
+    "image": "images/vinyl.jpg",
+    "title": "Moanin'",
+    "price": "40$"
+  },
 
-    {
-        "id": 3,
-        "image": "https://i.discogs.com/2qhXeTo4drpHNpzqAWc_jpzfDV1wQrfWp7qUCzCi_Cc/rs:fit/g:sm/q:40/h:300/w:300/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9BLTYyNTAz/NjEtMTYyNTI2MTI2/Ni04NTg4LmpwZWc.jpeg",
-        "title": "My Blade",
-        "price": "20$"
-    },
+  {
+    "id": 6,
+    "image": "https://i.discogs.com/WIcmVaAsfOwxgaKApPMciVitogXJEkSAl1TyIhxTNRw/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTE1NDk2/MzYtMTI2NTk0ODA5/NC5qcGVn.jpeg",
+    "title": "Master of Puppets",
+    "price": "20$"
+  },
 
-    {
-        "id": 4,
-        "image": "https://i.discogs.com/GozxOkL1APz4EBJeauQ0Gvjk0Nmu7NthCsbCvplL4p4/rs:fit/g:sm/q:90/h:400/w:400/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTE1NzI4/NjMzLTE1OTY3MDA3/ODQtNDk5NS5qcGVn.jpeg",
-        "title": "Hidratado pero Mesa",
-        "price": "20$"
-    },
+  {
+    "id": 7,
+    "image": "https://i.discogs.com/J5KwUEiruM9nFJ2M8i5Ru8q-O9zKhPDeenbSoEpuiBE/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTM0NDc3/MTItMTQ1NjkxMjMz/NS05NzQzLmpwZWc.jpeg",
+    "title": "Estopa",
+    "price": "25$"
+  },
+  {
+    "id": 8,
+    "image": "https://i.discogs.com/_QmUgeMNOZ6HPKn242RWSoor2bOgoe5Wz4LyRQT-KKo/rs:fit/g:sm/q:90/h:598/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTM4MDk4/MC0xNDM4NDYxMzcy/LTQxNDIuanBlZw.jpeg",
+    "title": "Highway to Hell",
+    "price": "69$"
+  },
+
+  {
+    "id": 9,
+    "image": "https://i.discogs.com/zvdXRD6psM_VkGLam4euRJIZ7o-fq1o8c6zwRlafsM8/rs:fit/g:sm/q:90/h:597/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTEyMTQz/MTYtMTIyNTA2NDI2/OS5qcGVn.jpeg",
+    "title": "Everything goes numb",
+    "price": "30$"
+  },
+  {
+    "id": 10,
+    "image": "https://i.discogs.com/wjjMk1xCKdaPbsUbaXWzHBNi3K-fmt3CYkQGqt0c_Z8/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTYyNzYx/ODMtMTY3NzAwMzM5/Ni0yMDQ1LmpwZWc.jpeg",
+    "title": "Kind of Blue",
+    "price": "55$"
+  },
+
+  {
+    "id": 3,
+    "image": "https://i.discogs.com/2qhXeTo4drpHNpzqAWc_jpzfDV1wQrfWp7qUCzCi_Cc/rs:fit/g:sm/q:40/h:300/w:300/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9BLTYyNTAz/NjEtMTYyNTI2MTI2/Ni04NTg4LmpwZWc.jpeg",
+    "title": "My Blade",
+    "price": "10$"
+  },
+
+  {
+    "id": 11,
+    "image": "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/81o6k0bPesL._SL1425_.jpg",
+    "title": "The Black Parade",
+    "price": "15$"
+  },
+  {
+    "id": 12,
+    "image": "https://i.discogs.com/OXa5t4aczEk-YiQFOyEOuYeY2WTuPrzGsVxgsx9T1ig/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTI0ODc4/NDg2LTE2NjYzNTc4/MzgtODc5NC5qcGVn.jpeg",
+    "title": "Midnights",
+    "price": "75$"
+  },
+  {
+    "id": 13,
+    "image": "https://i.discogs.com/Y0dubrWCAJsnPJfUINdSvQRupz8sIdRJz8cv-XFhL1I/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTE4OTU1/MDItMTM5OTE5NzM1/My05NDgyLmpwZWc.jpeg",
+    "title": "Humbug",
+    "price": "20$"
+  },
+
+  {
+    "id": 4,
+    "image": "https://i.discogs.com/GozxOkL1APz4EBJeauQ0Gvjk0Nmu7NthCsbCvplL4p4/rs:fit/g:sm/q:90/h:400/w:400/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTE1NzI4/NjMzLTE1OTY3MDA3/ODQtNDk5NS5qcGVn.jpeg",
+    "title": "Hidratado pero Mesa",
+    "price": "15$"
+  },
+  {
+    "id": 14,
+    "image": "https://i.discogs.com/-lQEjzCDqCQ_jNOEFpBZeSoQY-JFf03oNq17M1gpYcU/rs:fit/g:sm/q:90/h:580/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTM0NjA5/NTctMTMzMTI2NzI4/NS5qcGVn.jpeg",
+    "title": "Diamond Life",
+    "price": "45$"
+  },
+  {
+    "id": 15,
+    "image": "https://i.discogs.com/6dBCd5ilQQQRBGf1p6kEjzATY8EdRgEqz2e8J80fjGw/rs:fit/g:sm/q:90/h:475/w:485/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTU2NjQ1/NTMtMTM5OTMxNjkw/NS03NjgwLmpwZWc.jpeg",
+    "title": "Yo, minoría absoluta",
+    "price": "15$"
+  },
+  {
+    "id": 16,
+    "image": "https://i.discogs.com/YzhtLts7LkthVlFHqcaq5ZNZ59GEwslZA9KCw556CSc/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTM1NTIz/My0xNDI2MTY2MTc0/LTkwODYuanBlZw.jpeg",
+    "title": "Songs in the key of life",
+    "price": "32$"
+  },
+  {
+    "id": 17,
+    "image": "https://i.scdn.co/image/ab67616d0000b27363e163ead118db13035c323f",
+    "title": "Ira dei",
+    "price": "12$"
+  },
+
+
+  {
+    "id": 2,
+    "image": "https://i.discogs.com/BLb2qpCGiJ90OLh389AwftGVpJC3_S2UyzM-OJtnRKc/rs:fit/g:sm/q:90/h:598/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTEyODM2/NzI4LTE1ODc4MjQw/MDUtMzU5Mi5wbmc.jpeg",
+    "title": "Delta's fury",
+    "price": "30$"
+  },
+
+  {
+    "id": 18,
+    "image": "https://i.discogs.com/UzOVsidig6oqt8HnKyd3JBzigVLyWuc_J3OYOqvU6ME/rs:fit/g:sm/q:90/h:300/w:300/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTQyMTI3/MjItMTM1ODY5Njk0/OC02ODMzLmpwZWc.jpeg",
+    "title": "Bone Structure",
+    "price": "25$"
+  },
+  {
+    "id": 19,
+    "image": "https://i.discogs.com/pU2rfxJKyHRPQofpc2vFX3dzZ7i_kje9HdRQb6g3psM/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTM2OTQw/OC0xMjQ1MzgwNTQw/LmpwZWc.jpeg",
+    "title": "Hybrid Theory",
+    "price": "15$"
+  },
+  {
+    "id": 20,
+    "image": "https://i.discogs.com/fZJ1BSdJ4r8Pz6rr8m6dJ8VCnqs3XAEB3ElkYRrGads/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTg0NDkw/NC0xMTY0NzEyNzc4/LmpwZWc.jpeg",
+    "title": "Demon Days",
+    "price": "23$"
+  }
+
 
 ];
 const URL = new URLSearchParams(window.location.search);
@@ -34,77 +138,77 @@ const search = URL.get('search');
 console.log(search);
 
 let cartItems = sessionStorage.getItem('discos');
-if(cartItems == null) {
+if (cartItems == null) {
   cartItems = '';
 }
 
 const container = document.getElementById("vinyls-container");
 
 vinyls.forEach((vinyl) => {
-    const colDiv = document.createElement('div');
-    colDiv.classList.add('col');
+  const colDiv = document.createElement('div');
+  colDiv.classList.add('col');
 
-    const cardDiv = document.createElement('div');
-    cardDiv.classList.add('card');
-    cardDiv.classList.add('bg-dark');
-    cardDiv.classList.add('text-white');
-    cardDiv.setAttribute('id', vinyl.id);
+  const cardDiv = document.createElement('div');
+  cardDiv.classList.add('card');
+  cardDiv.classList.add('bg-dark');
+  cardDiv.classList.add('text-white');
+  cardDiv.setAttribute('id', vinyl.id);
 
 
-    const cardBodyDiv = document.createElement('div');
-    cardBodyDiv.classList.add('card-body');
+  const cardBodyDiv = document.createElement('div');
+  cardBodyDiv.classList.add('card-body');
 
-    const img = document.createElement("img");
-    img.src = vinyl.image;
-    img.classList.add("card-img-top");
-    img.setAttribute("alt", "...");
+  const img = document.createElement("img");
+  img.src = vinyl.image;
+  img.classList.add("card-img-top");
+  img.setAttribute("alt", "...");
 
-    const title = document.createElement('h5');
-    title.classList.add('card-title');
-    title.innerText = vinyl.title;
+  const title = document.createElement('h5');
+  title.classList.add('card-title');
+  title.innerText = vinyl.title;
 
-    const price = document.createElement('p');
-    price.classList.add('card-text');
-    price.innerText = vinyl.price;
+  const price = document.createElement('p');
+  price.classList.add('card-text');
+  price.innerText = vinyl.price;
 
-    const addCartButton = document.createElement('a');
-    addCartButton.setAttribute("href", "#");
-    addCartButton.addEventListener("click", function(){
-        cartItems += vinyl.id+"+";
-    });
-    addCartButton.classList.add('btn');
-    addCartButton.classList.add('btn-warning');
-    addCartButton.innerText = 'Add to Cart';
+  const addCartButton = document.createElement('a');
+  addCartButton.setAttribute("href", "#");
+  addCartButton.addEventListener("click", function () {
+    cartItems += vinyl.id + "+";
+  });
+  addCartButton.classList.add('btn');
+  addCartButton.classList.add('btn-warning');
+  addCartButton.innerText = 'Add to Cart';
 
-    cardDiv.appendChild(img);
-    cardBodyDiv.appendChild(title);
-    cardBodyDiv.appendChild(price);
-    cardBodyDiv.appendChild(addCartButton);
-    cardDiv.appendChild(cardBodyDiv);
-    colDiv.appendChild(cardDiv);
-    container.appendChild(colDiv);
+  cardDiv.appendChild(img);
+  cardBodyDiv.appendChild(title);
+  cardBodyDiv.appendChild(price);
+  cardBodyDiv.appendChild(addCartButton);
+  cardDiv.appendChild(cardBodyDiv);
+  colDiv.appendChild(cardDiv);
+  container.appendChild(colDiv);
 
-    addCartButton.addEventListener("click",function() {
-      addCartButton.classList.remove("btn-warning");
-      addCartButton.innerText = "Item added!";
-      addCartButton.classList.add("btn-success");
-    });
+  addCartButton.addEventListener("click", function () {
+    addCartButton.classList.remove("btn-warning");
+    addCartButton.innerText = "Item added!";
+    addCartButton.classList.add("btn-success");
+  });
 
 });
 
 const searchInput = document.getElementById("searchInput");
-if(search!=null){
-    /*searchInput.value = search;
-    //searchInput.dispatchEvent(new Event('input'));
-    searchInput.dispatchEvent(new Event('input', { bubbles: true }));*/
-    setTimeout(() => {
-        searchInput.value = search;
-        searchInput.dispatchEvent(new Event('input'));
-      }, 0);
+if (search != null) {
+  /*searchInput.value = search;
+  //searchInput.dispatchEvent(new Event('input'));
+  searchInput.dispatchEvent(new Event('input', { bubbles: true }));*/
+  setTimeout(() => {
+    searchInput.value = search;
+    searchInput.dispatchEvent(new Event('input'));
+  }, 0);
 }
 
 
-searchInput.addEventListener('input', function() {
+searchInput.addEventListener('input', function () {
   // coge el término buscado
   const searchTerm = this.value.toLowerCase();
 
@@ -112,7 +216,7 @@ searchInput.addEventListener('input', function() {
   const filteredVinyls = vinyls.filter(vinyl => vinyl.title.toLowerCase().includes(searchTerm));
 
   const vinylsContainer = document.querySelector('#vinyls-container');
-    console.log(filteredVinyls);
+  console.log(filteredVinyls);
   vinylsContainer.innerHTML = '';
 
   // html para crear los vinilos
@@ -145,8 +249,8 @@ searchInput.addEventListener('input', function() {
 
     const addCartButton = document.createElement('a');
     addCartButton.setAttribute("href", "#");
-    addCartButton.addEventListener("click", function(){
-        cartItems += vinyl.id+"+";
+    addCartButton.addEventListener("click", function () {
+      cartItems += vinyl.id + "+";
     });
     addCartButton.classList.add('btn');
     addCartButton.classList.add('btn-warning');
@@ -160,19 +264,19 @@ searchInput.addEventListener('input', function() {
     colDiv.appendChild(cardDiv);
     container.appendChild(colDiv);
 
-    addCartButton.addEventListener("click",function() {
+    addCartButton.addEventListener("click", function () {
       addCartButton.classList.remove("btn-warning");
       addCartButton.innerText = "Item added!";
       addCartButton.classList.add("btn-success");
-    });    
+    });
   });
 });
 
 
 const cartButton = document.getElementById("cart");
-cartButton.addEventListener("click", function(){
-    location.href="cart.html";
-    sessionStorage.setItem('discos',cartItems);
+cartButton.addEventListener("click", function () {
+  location.href = "cart.html";
+  sessionStorage.setItem('discos', cartItems);
 });
 // console.log(vinyls);
 
