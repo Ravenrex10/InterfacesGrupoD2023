f186e18daa8eeb002b92e14fbb7309c9e3a36aa0
dani-dlg <100839577+dani-dlg@users.noreply.github.com>
2023-04-12 09:26:48 +0200

    enter para buscar

--- Skiffle/js/home.js
+++ Skiffle/js/home.js
@@ -5,6 +5,12 @@ const vinyl = document.getElementById("recomendedVinyl");
 searchButton.addEventListener("click", search, false);
 vinyl.addEventListener("click", recomendedVinyl, false);
 
+searchInput.addEventListener("keydown", function(e) {
+    if (e.key === "Enter") {
+        search();
+    }
+}, false);
+
 function search(){
     window.location.href="shop.html?search="+searchInput.value;
 }
